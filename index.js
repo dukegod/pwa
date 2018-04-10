95aa6f5d25c0099291bc679c047a45161cfe1d3a
liuhui <liuhui15@jd.com>
2018-04-10 16:48:47 +0800

    add compression

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
+var compression = require('compression')
 var app = express()
 
+app.use(compression());
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/build'))
 
--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to ReactPWA</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          pwa是一个很有场景的工具
         </p>
+        <section>
+          
+        </section>
       </div>
     );
   }

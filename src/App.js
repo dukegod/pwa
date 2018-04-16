01c3d5914b2fdf05e80a3bf668ca6bcf891f3c37
dukegod <dukehui1991@gmail.com>
2018-04-16 14:45:11 +0800

    image

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to ReactPWA</h1>
+          <h1 className="App-title">PWA</h1>
         </header>
         <p className="App-intro">
-          pwa是一个很有场景的工具
+          progressive web application
         </p>
         <section>
-          
+          introduce some tips for new comer！
         </section>
       </div>
     );
